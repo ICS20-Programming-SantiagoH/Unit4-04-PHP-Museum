f4bfa85ca466594e1686886fc27277a8d66433b7
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-04-30 20:58:13 +0000

    final

--- results.php
+++ results.php
@@ -4,21 +4,27 @@
 $age = $_POST['age'];
 $dayOfTheWeek = $_POST['dayOfTheWeek'];
 
-//Display if user can watch r rated movies
-if ($age <= 5 || $age >= 95) {
-  echo "You can go enter for free.";
+//Display if user can enter for free
+if ($age <= 5 && $age >= 0 || $age >= 95 && $age <= 120) {
+  echo "You can enter for free.";
 
 }
 
-  // Display if user can watch G rated movies
+  // Display if user gets a discount
 else if ($age >= 12 && $age <= 21 || $dayOfTheWeek == "Tuesday" || $dayOfTheWeek == "Thursday") {
   echo "You get the student discount.";
 
 }
 
+  // Display if user has to pay full price
+else if ($age >= 0 && $age <=120) {
+  echo "You have to pay full price.";
+
+}
+  
 // Display is the user does not enter a valid age
 else {
-  echo "You have to pay full price.";    
+  echo "Enter a valid age.";    
 }
 
 ?>
